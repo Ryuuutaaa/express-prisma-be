f3f91b8883610224d76b763e4e894d8593dacb2f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-09-15 17:36:04 +0800

    feat: add setup router controller

---
+++ backend/routes/ProductRoute.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { getProducts,getProductById,createProduct,updateProduct,deleteProduct } from "../controllers/ProductController"
+
+const router = express.Router()
+
+router.get("/products", getProducts)
+router.get("/products/:id", getProductById)
+router.get("/products", createProduct)
+router.get("/products/:id", updateProduct)
+router.get("/products/:id", deleteProduct)
+
+export default router
