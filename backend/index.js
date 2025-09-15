e4afd3c453bab5d29f31f15390aeaf6413b4dcb5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-09-15 17:38:14 +0800

    feat: add product route setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import dotent from "dotenv"
+import ProductRoute from "./routes/ProductRoute"
 dotent.config()
 
 const app  = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(ProductRoute);
 
 app.listen(process.env.APP_PORT, () => {
   console.log('server up and running  ')
