e7983b8122f0e03e0ce39a0bd2774781e1f4e979
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-09-18 12:44:47 +0800

    feat: add get producct and get product by id

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,9 +1,26 @@
-export const getProducts = (req, res) => {
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export const getProducts = async (req, res) => {
+  try{
+    const response = await prisma.product.findMany();
+    res.status(200).json(response)
+  }catch(error){
+    res.status(500).json({msg: error.message})
+  }
 
 }
 
-export const getProductById = (req, res) => {
-
+export const getProductById = async (req, res) => {
+  try{
+    const response = await prisma.product.findUnique({
+      where : {
+        id: Number(req.params.id)
+      }
+    })
+  }catch(error){
+    res.status(500).json({msg: error.message})
+  }
 }
 
 export const createProduct = (req, res) => {
